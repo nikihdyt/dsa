55bfc6394c6a6a3d2bbfbec5aeaa20c10b2d8079
nikihdyt <nikihidayati20@gmail.com>
2025-01-07 13:30:05 +0800

    07/365

--- main.go
+++ main.go
@@ -11,14 +11,12 @@ func main() {
 	c := []int{9,9}
 	d := []int{8,9,9,9}
 	e := []int{9,8,7,6,5,4,3,2,1,0}
-	f := []int{9,8,9}
 	g := []int{9,8,9}
 	fmt.Println(solution.PlusOne5thTry(a))
 	fmt.Println(solution.PlusOne5thTry(b))
 	fmt.Println(solution.PlusOne5thTry(c))
 	fmt.Println(solution.PlusOne5thTry(d))
 	fmt.Println(solution.PlusOne5thTry(e))
-	println("--- test case [9,8,9] ---")
-	fmt.Println(solution.PlusOne4thTry(f))
 	fmt.Println(solution.PlusOne5thTry(g))
+	fmt.Println(solution.PlusOne5thTry([]int{9}))
 }
--- solution/5plusOne.go
+++ solution/5plusOne.go
@@ -86,5 +86,27 @@ func PlusOne4thTry(digits []int) []int { // failed in test case [9,8,9]
 }
 
 func PlusOne5thTry(digits []int) []int {
+	if digits[len(digits)-1] != 9 {
+		digits[len(digits)-1]++
+		return digits
+	}
 	
+	for i := 0; i < len(digits); i++ { // digits[last] == 9  // i = index
+		observedIdx := len(digits) - i - 1 // looping dari belakang ke depan
+
+		digits[observedIdx]++ // digits[last_idx] pasti akan 10
+
+		if digits[observedIdx] == 10 && observedIdx != 0 {
+			digits[observedIdx] = 0
+		} else {
+			break
+		}
+	}
+
+	if digits[0] == 10 {
+		digits = append([]int{1}, digits...)
+		digits[1] = 0
+	}
+
+	return digits
 }
