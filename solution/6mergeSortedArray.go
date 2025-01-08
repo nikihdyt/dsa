66c1abac8f6cf4249a704f18cfd7af1be8331b5c
nikihdyt <nikihidayati20@gmail.com>
2025-01-08 20:42:50 +0800

    08/365 huhu :(

---
+++ solution/6mergeSortedArray.go
@@ -0,0 +1,13 @@
+package solution
+
+// merge 2 array in a non-decreasing order
+// store in the nums1
+
+func MergeSortedArray(nums1 []int, m int, nums2 []int, n int) []int {
+	pM := m-1
+	pN := n-1
+	x := len(nums1)-1
+	
+
+	return []int{}
+}
